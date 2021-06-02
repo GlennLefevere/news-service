f1dcfa8261531b1fd9a7175ac0bd70f29fc58fac
Glenn Lefevere <glennlefevere@hotmail.com>
2021-06-02 14:23:24 +0200

    authentication setup

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import * as news from './news';
+import * as user from './user';
+import basicAuth from 'express-basic-auth';
 
 const app = express();
 
 app.use(cors());
 
+app.use(basicAuth({
+    users: {
+        'admin': 'admin-123',
+        'user': 'user-123',
+    }
+}));
+
+app.use('/user', user.router);
 app.use('/news', news.router);
 
 const port = process.env.port || 8080;
---
+++ user.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+
+export const router = express.Router();
+
+const admin = {
+    firstName: 'Jos',
+    lastName: 'Verstappen',
+    email: 'Jos.de.bos@verstappen.nl',
+    authorities: ["ROLE_ADMIN", "ROLE_USER"]
+}
+
+const basic = {
+    firstName: 'Max',
+    lastName: 'Verstappen',
+    email: 'max@verstappen.nl',
+    authorities: ["ROLE_USER"]
+}
+
+const users: { [id: string]: any } = {
+    admin,
+    user: basic,
+}
+
+router.get('/', (req, res) => {
+    const {user} = (req as any).auth;
+    res.send(users[user]);
+});
